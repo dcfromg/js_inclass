3e781de8c583df400fc50c5a1971074e91d7e469
David Cox <143331446+dcfromg@users.noreply.github.com>
2024-04-04 15:57:58 -0400

    Add files via upload

---
+++ js/jsChallenges2.js
@@ -0,0 +1,18 @@
+document.querySelector("#sameAddress").addEventListener("click"), function () {
+  let bill = document.querySelector("#bill");
+  let home = document.querySelector("#home");
+
+  if (this.checked) {
+    home.value = bill.value;
+    home.disabled = true;
+  }
+  else {
+    home.value = "";
+    home.disabled = false;
+  }
+
+
+
+
+}
+
